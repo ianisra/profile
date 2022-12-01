b757e81849570cc4d82bab378ac4d23bc04755ba
ianisra <90713096+ianisra@users.noreply.github.com>
2022-12-01 16:09:36 -0600

    Update index.rb

--- controls/index.rb
+++ controls/index.rb
@@ -1,1 +1,7 @@
-
+control 'index' do
+  impact 'medium'
+  title 'HTTP index page should have the following text: “Welcome to my SSO class" '
+  describe http('http://localhost') do
+    its('body') { should eq 'Welcome to my SSO class 😘' }
+  end
+end
