d1c9c41180431d4f759575faf82baf9b5fb65fa1
ianisra <90713096+ianisra@users.noreply.github.com>
2022-12-01 16:52:48 -0600

    Update ports.rb

--- controls/ports.rb
+++ controls/ports.rb
@@ -1,6 +1,7 @@
 control 'ports' do
   impact 'medium'
   title 'Port 80, 443 should be reachable'
+  desc 'Se revisa que esten habilitados los puertos 80 y 443'
   describe apache_conf do
   its('Listen') { should =~ [ '80', '443' ] }
   end
