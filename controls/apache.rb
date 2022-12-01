93c972d607d572a32d3e03f54c807d2579d3fac9
ianisra <90713096+ianisra@users.noreply.github.com>
2022-12-01 16:45:59 -0600

    Update apache.rb

--- controls/apache.rb
+++ controls/apache.rb
@@ -1,9 +1,9 @@
 control 'apache' do
   impact 'medium'
   title 'Apache2 should be configured and running'
-  describe service(apache.service) do
-    it { should be_enabled }
-    it { should be_running }
+  describe service 'httpd' do
+  it { should be_enabled }
+  it { should be_running }
   end
 end
 
